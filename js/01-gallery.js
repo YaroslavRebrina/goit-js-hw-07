3d913db3c5ed9b5d0c46647358a5e27f71bab4a6
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-02-11 12:26:43 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.innerHTML = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt = "${description}"
+            />
+        </a>
+    </div>`
+  )
+  .join("");
+
+const onImgClick = (e) => {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}">)`
+  );
+
+  instance.show();
+};
+
+galleryRef.addEventListener("click", onImgClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryRefItem = document.querySelector(".gallery__item");
+
+galleryRef.innerHTML = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li>
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`
+  )
+  .join("");
+
+const onImgClick = (e) => {
+  if (e.target.nodeName !== "IMG") return;
+
+  let lightbox = new SimpleLightbox(`.gallery__item`, {
+    captionsData: `alt`,
+    captionDelay: 250,
+    captionPosition: "bottom",
+  });
+};
+
+galleryRef.addEventListener("click", onImgClick);
